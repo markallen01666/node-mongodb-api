5839ebe2e928fdcc3573d1b2eed3c6e2e20d6c1a
Mark Allen <markallen.webdeveloper@gmail.com>
2019-10-01 11:40:02 +0100

    Working app tested locally

--- index.js
+++ index.js
@@ -3,25 +3,23 @@
 // M Allen - 2019
 
 const express = require('express')
-const app = new express()
+const app = express()
 const mongoose = require('mongoose')
-const Offices = require('./models/Offices.js')
-const Employees = require('./models/Employees.js')
 
 // controllers
 const getOfficesController = require('./controllers/getOfficesController')
 const getEmployeesController = require('./controllers/getEmployeesController')
 
 // connect database
-mongoose.connect('mongodb+srv://53ptolemy8607:n6SkRDlbTED6kPZN@cluster0-wvhp3.mongodb.net/Company', { // -- incomplete - needs more work
-  
+mongoose.connect('mongodb+srv://haleon55:gyc2eivi16mrejC7@cluster0-wvhp3.mongodb.net/company1', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+}, (error => {
+  console.log(`Mongoose connection error: ${error}`)
+}))
 
-app.get('/', (req, res) => {
-  res.json(data)
-})
+app.get('/employees', getEmployeesController)
+app.get('/offices', getOfficesController)
 
 // Port allocation and default for Heroku commit
 let port = process.env.PORT
